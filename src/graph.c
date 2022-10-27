4d06fcb4e6c2f9f9836fb597ee260ed18ddacb47
S-Pz <savio_fr@outlook.com>
2022-10-27 10:18:00 -0300

    commit in code

--- src/graph.c
+++ src/graph.c
@@ -3,6 +3,7 @@
  
 #include"graph.h"
 
+//Função ao qual realiza a montagem do grafo dinâmicamente
 int **montaGrafo(FILE *arq, int **grafo, int C){
     
   int px,py,d;
@@ -21,12 +22,13 @@ int **montaGrafo(FILE *arq, int **grafo, int C){
   return grafo; // retorna a grafo montado
 }
 
+//Função que realiza a limpeza no grafo
 void freeGraph(int **grafo, int P){
 
-    for(int i=0; i<P; i++){
-        free(grafo[i]);
-    }
-    free(grafo);
+  for(int i=0; i<P; i++){
+    free(grafo[i]);
+  }
+  free(grafo);
 }
 
 int **allocGraph(int P){
@@ -45,6 +47,7 @@ int **allocGraph(int P){
   return x;
 }
 
+//Função para limpar o grafo
 void printGraph(int  **graph,int P){
   
   for(int i= 0; i<P;i++){
@@ -57,30 +60,7 @@ void printGraph(int  **graph,int P){
   printf("\n\n");
 }
 
-// int **montaGrafo(FILE *arq, int **grafo, int C, Povo **lista){
-    
-//   int px,py,d;
-//   Povo *x,*y;
-  
-//   while (C != 0){
-
-//     fscanf(arq, "%d", &px); 
-//     fscanf(arq, "%d", &py); 
-//     fscanf(arq, "%d", &d); 
-
-//     grafo[px-1][py-1] = 1;    
-//     grafo[py-1][px-1] = 1;
-    
-//     x = searchList(lista,px);
-//     y = searchList(lista,py);
-
-//     //setDistanc(x,y,d);
-    
-//     C--;
-//   }
-//   return grafo; // retorna a grafo montado
-// }
-
+//Função que verifica se um detreminado povo possui caminho até o outro
 int searchGraph(int **graph, int from , int to){
   
   if(graph[from][to] == 1 && graph[to][from] == 1){
@@ -89,34 +69,3 @@ int searchGraph(int **graph, int from , int to){
     return 0;
   }
 }
-
-// void setDistanc(Povo *x, Povo *y, int dist){
-
-//   x->dist=dist;
-//   y->dist=dist;
-  
-// }
-
-// void BFS(int **graph,Povo **lista, int ini, int *visited){
-
-//   int vert, nVert, cont = 1;
-//   int *fila , iniFila=0, fimFila=0;
-//   Povo *aux;
-  
-//   for(int i= 0; i < 5; i++){
-//     visited[i] = 0;
-//   }
-//   nVert = 5;
-//   fila = (int*)malloc(nVert *sizeof(int));
-//   fimFila++;
-//   fila[fimFila] = ini;
-//   visited[ini]= cont;
-
-//   while(iniFila!=fimFila){
-//     iniFila = (iniFila + 1) % nVert;
-//     vert = fila[iniFila];
-//     cont++;
-//     for(int j =0 ) 
-//   }
-
-// }
